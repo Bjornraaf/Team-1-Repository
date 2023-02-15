3559680f3acb56cb2f01a01f478daa6e1cf53a55
Ties Postma <54939869+ties-postma@users.noreply.github.com>
2023-02-15 15:17:12 +0100

    shooting mechanic in the making
    started on a shooting mechanic by adding targets and a shooting mechanism, the shooting script has no way of being activated yet.

---
+++ Assets/TargetScripts/Shootscript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootscript : MonoBehaviour
+{
+    public GameObject ARCamera;
+    public GameObject particle;
+
+    public void shoot()
+    {
+            RaycastHit hit;
+        
+        if (Physics.Raycast(ARCamera.transform.position, ARCamera.transform.forward, out hit))
+        {
+            if (hit.transform.name == "Sphere 1(clone)"    || hit.transform.name == "Sphere 2(clone)"   || hit.transform.name == "Sphere 3(clone)")
+            {
+                Destroy(hit.transform.gameObject);
+                Instantiate(particle, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
