269312dff101c491949126a7a9f2fab0ffbb4bc6
Ties-Postma <28114@ma-web.nl>
2023-02-22 15:38:26 +0100

    changed some small things

--- Assets/TargetScripts/Shootscript.cs
+++ Assets/TargetScripts/Shootscript.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 
 public class Shootscript : MonoBehaviour
 {
-    public GameObject arCamera; // so you can add the camera to raycast
-    public GameObject particle; // so you can choose what particle to play
-    public Text scoreText; // so you can see your score
-    public Text scoreText2; // so you can see your score at the end
+    [SerializeField] private GameObject arCamera; // so you can add the camera to raycast
+    [SerializeField] private GameObject particle; // so you can choose what particle to play
+    [SerializeField] private Text scoreText; // so you can see your score
+    [SerializeField] private  Text scoreText2; // so you can see your score at the end
      int score = 0;
 
-    public void shoot() //takes care of the shooting mechanic
+    /// <summary>
+    /// this takes care of all the shooting by using the raycast of the camera to see if it hits the object with a specific name and adds a point to the player's score
+    /// </summary>
+    public void Shoot() //takes care of the shooting mechanic
     {
             RaycastHit hit;
         
