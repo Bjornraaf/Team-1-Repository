6b7338837c7eadd551c094e515c07a767c11321a
Bjornraaf <30087@ma-web.nl>
2023-02-23 10:45:11 +0100

    Trying to repair GPS Branch

--- Assets/Scripts/GPSSystem/GPSSystem.cs
+++ Assets/Scripts/GPSSystem/GPSSystem.cs
@@ -1,24 +1,27 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class GPSSystem : MonoBehaviour
 {
-    Vector2 r_Init;
-    Vector2 r_CurrentPostion;
-    Vector2 f_Init;
-    Vector2 f_CurrentPostion;
+    private Vector2 RealInit;
+    private Vector2 RealCurrentPostion;
+    private Vector2 FakeCurrentPostion;
 
-    public float scale;
-    public bool isFakingLocation;
+    [Header("Scaling")]
+    [SerializeField] private float Scale;
+    [Header("Test Location")]
+    [SerializeField] private bool isFakingLocation;
 
     private void Start()
     {
         Input.location.Start( 5, 10);
         Input.compass.enabled = true;
     }
+    /// <summary>
+    /// Holds all the failsafes for the game and the Fakelocation game tester.
+    /// </summary>
+    /// <returns> a new Setlocation every amount of meters defined by the Input.location.Start </returns>
     IEnumerator UpdatePostion()
     {
         if (isFakingLocation == false)
@@ -44,14 +47,13 @@ public class GPSSystem : MonoBehaviour
             if (Input.location.status == LocationServiceStatus.Failed)
             {
                 GameObject.Find("FailedText").GetComponent<Text>().text = "Location service status failed";
-                //Debug.Log("Location service status failed");
                 yield return null;
             }
             else
             {
-                if (r_Init == Vector2.zero)
+                if (RealInit == Vector2.zero)
                 {
-                    r_Init = new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude);
+                    RealInit = new Vector2(Input.location.lastData.latitude, Input.location.lastData.longitude);
                 }
                 SetLocation(Input.location.lastData.latitude, Input.location.lastData.longitude);
             }
@@ -61,17 +63,22 @@ public class GPSSystem : MonoBehaviour
             SetLocation(0 + Time.time, 0 + Time.time);
         }
     }
+
+    /// <summary>
+    /// Sets the postion of the player to a new location.
+    /// </summary>
+    /// <param name="latitude">Latitude is a coordinate that specifies the North–South position of a point on the surface of the Earth</param>
+    /// <param name="longitude">Longitude is a coordinate that specifies the East-West position of a point on the surface of the Earth</param>
     void SetLocation(float latitude, float longitude)
     {
-        r_CurrentPostion = new Vector2(latitude, longitude);
-        Vector2 delta = new Vector2(r_CurrentPostion.x - r_Init.x, r_CurrentPostion.y - r_Init.y);
-        f_CurrentPostion = delta * scale;
-        transform.position = new Vector3(f_CurrentPostion.x, 0, f_CurrentPostion.y);
+        RealCurrentPostion = new Vector2(latitude, longitude);
+        Vector2 delta = new Vector2(RealCurrentPostion.x - RealInit.x, RealCurrentPostion.y - RealInit.y);
+        FakeCurrentPostion = delta * Scale;
+        transform.position = new Vector3(FakeCurrentPostion.x, 0, FakeCurrentPostion.y);
         GameObject.Find("PositionText").GetComponent<Text>().text = transform.position.x + " : " + transform.position.y + " : " + transform.position.z;
     }
     private void Update()
     {
         StartCoroutine(UpdatePostion());
     }
-
 }
--- Assets/Scripts/GPSSystem/Waypoint.cs
+++ Assets/Scripts/GPSSystem/Waypoint.cs
@@ -1,14 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Waypoint : LevelLoader
+
+public class Waypoint : MonoBehaviour
 {
+    [SerializeField] private LevelLoader Loader;
+
     private void OnTriggerEnter(Collider other)
     {
-        LoadNextLevel();
+        Loader.LoadNextLevel();
     }
-
 }
