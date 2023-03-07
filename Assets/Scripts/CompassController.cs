a19c01eb634300aa90ee4c0f68e7aa896628ea09
PAP0 <29280@ma-web.nl>
2023-03-07 16:14:05 +0100

    Compass fixes
    -compassHeading now works normally and stays within 0-360 range
    -directions work

--- Assets/Scripts/CompassController.cs
+++ Assets/Scripts/CompassController.cs
@@ -32,14 +32,13 @@ public class CompassController : MonoBehaviour
     public float filterFactor = 0.05f;
     [Tooltip("The direction text that displays the heading(N, W, E,S)")]
     public Text directionText;
-    
+
     private float rawHeading; // Raw compass heading value.
-    
     private string directionString = ""; // String that changes depending on the compass direction.
 
     void Start()
     {
-        Input.compass.enabled = true; //
+        Input.compass.enabled = true;
         Input.location.Start();
     }
 
@@ -47,7 +46,7 @@ public class CompassController : MonoBehaviour
     {
         // Assigns the raw heading to the phone's true heading.
         rawHeading = Input.compass.trueHeading;
-        
+
         // If the compass is on, continue running script.
         if (Input.compass.enabled)
         {
@@ -57,45 +56,43 @@ public class CompassController : MonoBehaviour
 
             // Apply a low-pass filter to smooth out the heading value.
             compassHeading = Mathf.LerpAngle(compassHeading, rawHeading, filterFactor);
-            compassHeading %= 360f;
-            if (compassHeading < 0f)
-            {
-                compassHeading += 360f;
-            }
+            compassHeading = (compassHeading + 360f) % 360f;
 
             // Determine the compass direction based on the heading value.
-            if (rawHeading >= 315 || rawHeading < 45)
+            if (compassHeading >= 315 || compassHeading < 45)
             {
                 directionString = "N";
             }
-            else if (rawHeading >= 45 && rawHeading < 135)
+            else if (compassHeading >= 45 && compassHeading < 135)
             {
                 directionString = "E";
             }
-            else if (rawHeading >= 135 && rawHeading < 225)
+            else if (compassHeading >= 135 && compassHeading < 225)
             {
                 directionString = "S";
             }
-            else if (rawHeading >= 225 && rawHeading < 315)
+            else if (compassHeading >= 225 && compassHeading < 315)
             {
                 directionString = "W";
             }
-            
+
             // Get the angle difference between the compass heading and the target direction.
             float angleDifference = Vector3.SignedAngle(transform.forward, targetDirection, Vector3.up);
 
-            // Snap the compass heading to 360 when rotating left
-            if (angleDifference < 0.1f)
+            // Wrap the angle difference to the -180 to 180 range.
+            angleDifference = (angleDifference + 180f) % 360f - 180f;
+
+            // Snap the compass heading to 0 or 360 when necessary.
+            if (compassHeading <= 0.1f || compassHeading >= 359.9f)
             {
-                compassHeading += 360f;
+                compassHeading = 0f;
             }
-            
 
             // Update the UI element to display the compass.
             compassUI.transform.rotation = Quaternion.Euler(0, 0, -compassHeading);
             compassUI.transform.GetChild(0).rotation = Quaternion.Euler(0, 0, angleDifference);
             directionText.text = "Direction: " + directionString;
-            
+
             // Log the heading and direction to the console.
             Debug.Log("Compass Heading: " + compassHeading.ToString("F2") + " degrees ");
             Debug.Log((rawHeading));
