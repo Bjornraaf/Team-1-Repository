c98fe56743bd9368e601ee0c6f0987f010df313a
Bjornraaf <30087@ma-web.nl>
2023-03-15 12:38:57 +0100

    Latest Changes to scenes

--- Assets/Scripts/SwapSceneWorm.cs
+++ Assets/Scripts/SwapSceneWorm.cs
@@ -1,11 +1,26 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwapSceneWorm : MonoBehaviour
 {
-    [SerializeField] private LevelLoader SceneLoader;  
+    //[Header("Animator")]
+    //[SerializeField] private Animator DTransition;
+    [SerializeField] private LevelLoader DLoader;
+    public void LoadGPS()
+    {
+        //StartCoroutine(DLoadLevel());
+    }
+    //IEnumerator DLoadLevel()
+    //{
+        //DTransition.SetTrigger("Start");
+        //yield return new WaitForSeconds(1);
+        //yield return null;
+    //    SceneManager.LoadScene("GPS");
+    //}
     public void NextGame(string SceneName)
     {
-        SceneLoader.loadSceneWithName = SceneName;
-        SceneLoader.LoadNextLevel();
+        DLoader.loadSceneWithName = SceneName;
+        DLoader.LoadNextLevel();
     }
 }
